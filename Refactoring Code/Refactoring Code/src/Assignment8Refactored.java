6feec4a38c2c6aa8c4b8f388ca9673f025aa68a9
JakeChris2002 <blankenshipjc02@gmail.com>
2025-10-19 17:26:34 -0500

    I changed the file name but left the old name in the code for context.

--- Refactoring Code/Refactoring Code/src/Assignment8Refactored.java
+++ Refactoring Code/Refactoring Code/src/Assignment8Refactored.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
-public class LegacyRoster {
+public class Assignment8Refactored {
 
     // --- Simple domain model ---
 
